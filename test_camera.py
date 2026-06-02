c3f33d9997bd150fc05a9ff526a131ed3594b164
poommynynano <52936@yupparaj.ac.th>
2026-06-02 12:59:35 +0700

    เตือน

--- test_camera.py
+++ test_camera.py
@@ -16,3 +16,63 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
+import cv2
+import time
+
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades +
+    'haarcascade_frontalface_default.xml'
+)
+
+cap = cv2.VideoCapture(0)
+
+last_seen = time.time()
+
+while True:
+
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5
+    )
+
+    if len(faces) > 0:
+        last_seen = time.time()
+
+        cv2.putText(
+            frame,
+            "Face Detected",
+            (20,50),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0,255,0),
+            2
+        )
+
+    else:
+
+        elapsed = time.time() - last_seen
+
+        if elapsed > 5:
+            cv2.putText(
+                frame,
+                "No Face Found!",
+                (20,50),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (0,0,255),
+                2
+            )
+
+    cv2.imshow("Camera", frame)
+
+    if cv2.waitKey(1) == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
